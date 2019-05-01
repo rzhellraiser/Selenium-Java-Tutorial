b7535d34f0860d0c8d865b5b0cd7e7eef7ad3da0
Rz <rz.dev.88@gmail.com>
2019-04-30 20:28:25 -0500

    Listener class, added a listener class and XMl file to explain how to
    report real time events when test starts, fails, passes, etc.
    
    Use of ITestListener to analyze test methods, perform logging, etc.

---
+++ TestNGSample/src/com/testng/listeners/DemoListeners.java
@@ -0,0 +1,53 @@
+ package com.testng.listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class DemoListeners implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Started : " + result.getName());
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Got Successful : " + result.getName());
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Got Failed : " + result.getName());
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Got Skipped : " + result.getName());
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
